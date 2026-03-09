a8d80ffac5f6e88e4f00ff7997261ccb03b2d5b0
DASKR <167687177+DASKR515@users.noreply.github.com>
2026-03-09 12:11:51 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,4 +1,13 @@
 
-$env:LUAROCKS_CONFIG = "C:\Program Files\lwa\etc\luarocks\config-5.4.lua"
-$env:PATH = "C:\Program Files\lwa\bin;" + $env:PATH
-luarocks --version
+$LWA_HOME = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
+
+
+$env:LUAROCKS_CONFIG = "$LWA_HOME\etc\luarocks\config-5.4.lua"
+$env:LUA_PATH = "$LWA_HOME\share\lua\5.4\?.lua;$LWA_HOME\share\lua\5.4\?\init.lua;;"
+$env:LUA_CPATH = "$LWA_HOME\lib\lua\5.4\?.dll;;"
+$env:PATH = "$LWA_HOME\bin;" + $env:PATH
+
+
+Write-Host "LWA Environment Active" -ForegroundColor Green
+Write-Host "Lua: $(lua54 -v)" -ForegroundColor Cyan
+Write-Host "LuaRocks: $(luarocks --version)" -ForegroundColor Cyan
